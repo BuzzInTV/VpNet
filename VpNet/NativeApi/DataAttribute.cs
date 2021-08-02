2b270a0e2583eb1df87a85ddaad9735b75cb71bb
Oliver Booth <me@olivr.me>
2021-08-03 00:17:08 +0100

    Remove XMLdoc from DataAttribute fields
    Addendum to 41a2cc50f5c42fa12acd6dddf6b86f33fe8c91f8

--- VpNet/NativeApi/DataAttribute.cs
+++ VpNet/NativeApi/DataAttribute.cs
@@ -2,13 +2,7 @@
 {
     internal enum DataAttribute
     {
-        /// <summary>
-        /// Object data
-        /// </summary>
 	    ObjectData = 0,
-        /// <summary>
-        /// The terrain node data
-        /// </summary>
         TerrainNodeData = 1
     }
 }
