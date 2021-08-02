2657ab5d67a912619fef67421ee19bcfb64f3e93
Oliver Booth <me@olivr.me>
2021-08-02 23:03:36 +0100

    Publicly expose Url/UriTarget

---
+++ VpNet/NativeApi/UriTarget.cs
@@ -0,0 +1,18 @@
+﻿namespace VpNet.NativeApi
+{
+    /// <summary>
+    ///     An enumeration of URI targets.
+    /// </summary>
+    public enum UriTarget
+    {
+        /// <summary>
+        ///     Opens the URI in an external browser.
+        /// </summary>
+        Browser,
+
+        /// <summary>
+        ///     Opens the URi in an internal browser, displayed as a 2D overlay on top of the 3D world view.
+        /// </summary>
+        Overlay
+    }
+}
