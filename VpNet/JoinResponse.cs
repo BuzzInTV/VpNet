342090043b9e385d76a74225e042bf2bf253d8eb
Oliver Booth <me@olivr.me>
2021-08-03 02:17:04 +0100

    Implement join/invite request models

---
+++ VpNet/JoinResponse.cs
@@ -0,0 +1,23 @@
+namespace VpNet
+{
+    /// <summary>
+    ///     An enumeration of join responses.
+    /// </summary>
+    public enum JoinResponse
+    {
+        /// <summary>
+        ///     The join request was accepted by the user.
+        /// </summary>
+        Accepted,
+
+        /// <summary>
+        ///     The join request was declined by the user.
+        /// </summary>
+        Declined,
+
+        /// <summary>
+        ///     The join request timed out.
+        /// </summary>
+        TimeOut
+    }
+}
---
+++ VpNet/JoinResult.cs
@@ -0,0 +1,28 @@
+﻿namespace VpNet
+{
+    /// <summary>
+    ///     Represents a join result.
+    /// </summary>
+    public readonly struct JoinResult
+    {
+        internal JoinResult(JoinResponse response, Location? location)
+        {
+            Response = response;
+            Location = location;
+        }
+
+        /// <summary>
+        ///     Gets the join location.
+        /// </summary>
+        /// <value>The join location. This value will be <see langword="null" /> if the request was declined, or timed out.</value>
+        public Location? Location { get; }
+
+        /// <summary>
+        ///     Gets the join response.
+        /// </summary>
+        /// <value>The join response.</value>
+        public JoinResponse Response { get; }
+
+        public static implicit operator bool(JoinResult result) => result.Response == JoinResponse.Accepted;
+    }
+}
