593bb6ce5754f328466031eb6e9c6404a282a02e
Oliver Booth <me@olivr.me>
2021-08-03 15:35:40 +0100

    Rewrite Join event args

---
+++ VpNet/EventData/JoinRequestReceivedEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace VpNet.EventData
+{
+    /// <summary>
+    ///     Provides event arguments for <see cref="VirtualParadiseClient.JoinRequestReceived" />.
+    /// </summary>
+    public sealed class JoinRequestReceivedEventArgs : EventArgs
+    {
+        /// <inheritdoc />
+        public JoinRequestReceivedEventArgs(JoinRequest joinRequest)
+        {
+            JoinRequest = joinRequest;
+        }
+
+        /// <summary>
+        ///     Gets the join request.
+        /// </summary>
+        /// <value>The join request.</value>
+        public JoinRequest JoinRequest { get; }
+    }
+}
